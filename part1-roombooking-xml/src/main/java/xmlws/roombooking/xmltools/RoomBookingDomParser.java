59785cdc9be944d82611148faa6349c1158bc23b
tanguyfenouillot <tanguy.fenouillot@iut-rodez.fr>
2020-03-04 17:59:43 +0100

    fix #1.2 Parser DOM

--- part1-roombooking-xml/src/main/java/xmlws/roombooking/xmltools/RoomBookingDomParser.java
+++ part1-roombooking-xml/src/main/java/xmlws/roombooking/xmltools/RoomBookingDomParser.java
@@ -5,18 +5,24 @@ import org.w3c.dom.Document;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
 
 public class RoomBookingDomParser implements RoomBookingParser {
 
-    private RoomBooking roomBooking;
+    private RoomBooking roomBooking = new RoomBooking();
+
+    private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss");
+
 
     public RoomBooking parse(InputStream inputStream) {
         try {
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder db = dbf.newDocumentBuilder();
             Document doc = db.parse(inputStream);
-            System.out.println(doc.getElementsByTagName("label").item(0).getTextContent());
-            System.out.println(doc.getElementsByTagName("username").item(0).getTextContent());
+            roomBooking.setRoomLabel(doc.getElementsByTagName("label").item(0).getTextContent());
+            roomBooking.setUsername(doc.getElementsByTagName("username").item(0).getTextContent());
+            roomBooking.setStartDate(sdf.parse(doc.getElementsByTagName("startDate").item(0).getTextContent()));
+            roomBooking.setEndDate(sdf.parse(doc.getElementsByTagName("endDate").item(0).getTextContent()));
         } catch (Exception e) {
             e.printStackTrace();
         }
